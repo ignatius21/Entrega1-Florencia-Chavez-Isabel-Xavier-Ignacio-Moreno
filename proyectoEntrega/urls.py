87a39f540d51f9c3701635b0b8be84ebabad6532
theopharnell <98174756+theopharnell@users.noreply.github.com>
2022-01-21 19:38:02 -0300

    avance2

--- proyectoEntrega/urls.py
+++ proyectoEntrega/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('AppGrupo6/',include('AppGrupo6.urls')),
+    path('AppEntrega/',include('AppEntrega.urls')),
 ]
