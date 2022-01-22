8c013eacd0f622891b60f8945933d6d775645729
theopharnell <98174756+theopharnell@users.noreply.github.com>
2022-01-22 00:16:38 -0300

    nuevos cambios en las urls

--- AppEntrega/urls.py
+++ AppEntrega/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AppEntrega.views import inicio,productos,nosotros,contacto,servicios
 
 urlpatterns = [
-    path('',inicio,name='inicio'),
+    path('inicio',inicio,name='inicio'),
     path('productos',productos,name='productos'),
     path('nosotros',nosotros,name='nosotros'),
     path('contacto',contacto,name='contacto'),
