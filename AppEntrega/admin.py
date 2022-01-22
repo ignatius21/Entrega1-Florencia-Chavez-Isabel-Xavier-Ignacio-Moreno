dab91fbde93b3692949f237f1a976a8e4032a172
theopharnell <98174756+theopharnell@users.noreply.github.com>
2022-01-22 00:33:51 -0300

    se agregar nosotros

--- AppEntrega/admin.py
+++ AppEntrega/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import  *
 
 # Register your models here.
+
+admin.site.register(Contacto)
+admin.site.register(Nosotros)
+admin.site.register(Productos)
+admin.site.register(Servicios)
